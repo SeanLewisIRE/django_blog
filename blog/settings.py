35460019d86f87c090500935ee6766bd7c8ae51e
Sean Lewis <slewis100@gmail.com>
2020-05-04 14:21:11 +0000

    added templates, add and edit post ability

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+if os.path.exists('env.py'):
+    import env
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8=c&cpg#@!b%@0u*@m0z(ad%pdk!t#f50-^eg07!juq$l9lr*#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
